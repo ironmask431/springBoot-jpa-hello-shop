4cc264288f6403f43b754af5b637076b55288df9
ironmask431 <ironmask431@gmail.com>
2022-03-15 22:35:27 +0900

    2022.03.15 - lombok테스트, @Slf4j 로그테스트

--- src/main/java/com/basic/Application.java
+++ src/main/java/com/basic/Application.java
@@ -1,4 +1,4 @@
-package com.restApi;
+package com.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/basic/controller/HomeController.java
+++ src/main/java/com/basic/controller/HomeController.java
@@ -1,5 +1,6 @@
-package com.restApi.controller;
+package com.basic.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +9,17 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+@Slf4j
 @Controller
 public class HomeController {
-
     @GetMapping
     public String home(Model model, Locale locale){
-
+        log.info("Welcome home! The client locale is "+locale+".");
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 h시 m분 s초");
-        String serverTime = now.format(formatter);
-        model.addAttribute("serverTime",now);
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년M월d일(E) a h시m분s초");
+        String format_now = now.format(formatter);
+        log.info("format_now="+format_now);
+        model.addAttribute("serverTime",format_now);
         return "home";
     }
 }

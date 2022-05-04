9a734429645afbf30690a844d8d0b78066fe244c
ironmask431 <ironmask431@gmail.com>
2022-05-04 21:21:13 +0900

    프로젝트 수정

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,11 @@
+import com.leesh.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+    @Test
+    public void contextLoads(){
+
+    }
+}

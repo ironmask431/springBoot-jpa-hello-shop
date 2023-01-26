d935937fa0d535baab870793798cf819ab180e98
ironmask431 <ironmask431@gmail.com>
2023-01-26 13:09:04 +0900

    엔티티 개발중

--- src/main/java/com/leesh/domains/Delivery.java
+++ src/main/java/com/leesh/domains/Delivery.java
@@ -1,4 +1,24 @@
 package com.leesh.domains;
 
+import lombok.Getter;
+import org.springframework.aop.framework.adapter.AdvisorAdapterRegistrationManager;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name="delivery_id")
+    private Long id;
+
+    //private Order order;
+
+    @Embedded //@Embeddable 내장타입 클래스 사용시 붙여줌.
+    private Address address;
+
+    //private DeliveryStatus deliveryStatus;
+
 }

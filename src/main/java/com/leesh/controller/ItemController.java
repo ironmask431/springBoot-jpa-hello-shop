8bf20fd895094b0a7f1cd0f5000b99cbd72c0b3b
ironmask431 <ironmask431@gmail.com>
2023-02-17 23:25:48 +0900

    상품 수정 API, 뷰

--- src/main/java/com/leesh/controller/ItemController.java
+++ src/main/java/com/leesh/controller/ItemController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -25,14 +27,13 @@ public class ItemController {
     }
 
     @PostMapping("/items/new")
-    public String create(BookForm bookForm){
+    public String create(BookForm form){
         Book book = new Book();
-        book.setName(bookForm.getName());
-        book.setPrice(bookForm.getPrice());
-        book.setStockQuantity(bookForm.getStockQuantity());
-        book.setAuthor(bookForm.getAuthor());
-        book.setIsbn(book.getIsbn());
-
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
         itemService.save(book);
         return "redirect:/";
     }
@@ -42,5 +43,33 @@ public class ItemController {
         List<Item> items = itemService.finaAll();
         model.addAttribute("items",items);
         return "items/itemList";
+    }
+
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable Long itemId, Model model){
+        Book book = (Book)itemService.findOne(itemId);
+        BookForm form = new BookForm();
+        form.setId(book.getId());
+        form.setStockQuantity(book.getStockQuantity());
+        form.setAuthor(book.getAuthor());
+        form.setIsbn(book.getIsbn());
+        form.setName(book.getName());
+        form.setPrice(book.getPrice());
+
+        model.addAttribute("form",form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.save(book);
+        return "redirect:/items";
     }
 }

6330bb376466e665e44f385a42e8aea00dc2e6d4
ironmask431 <ironmask431@gmail.com>
2023-02-18 17:17:02 +0900

    주문하기 api, 뷰 완료

--- src/main/java/com/leesh/controller/OrderController.java
+++ src/main/java/com/leesh/controller/OrderController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,5 +31,15 @@ public class OrderController {
         model.addAttribute("items",items);
 
         return "/order/orderForm";
+    }
+
+    //Post매핑의 경우 @RequestParam으로 필드를 각각 매핑 받거나
+    //@RequestBody 를 사용해 dto로 매핑 받을 수 있다.
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+        orderService.order(memberId, itemId, count);
+        return "redirect:/";
     }
 }
